629655590e3c57524b1f55e9aba0e01f90b4aed9
griffinGC <griffinDouble@gmail.com>
2019-04-08 04:45:35 +0900

    almost_complete

--- using_crawler_request.js
+++ using_crawler_request.js
@@ -7,54 +7,74 @@ var url = "http://www.ajou.ac.kr/main/life/food.jsp";
 
 //enter를 없애기 위해서 삽입
 const enterReg = /(\r\n|\n|\r)/gm;
-let stuDiner;
-let stuDinerL ="";
-let stuDinerD ="";
-let stuDinerS ="";
+let unionDiner;
 
 let domDiner;
-let domDinerT;
-let domDinerM ="";
-let domDinerL ="";
-let domDinerD ="";
 
 let officeDiner;
-let officeDiner1L ="";
-let officeDiner1D ="";
 
 let officeDiner2;
 
 var strArray;
-// const dinerList = [stuDiner, domDiner, officeDiner1, officeDiner2];
+// const dinerList = [unionDiner, domDiner, officeDiner1, officeDiner2];
 
+//식단 찾는것 자체를 함수화
 function findDiner(diner){
+    let stuDinerM ="";
+    let stuDinerL ="";
+    let stuDinerD ="";
+    let stuDinerS ="";
+
     if(diner.indexOf('등록된 식단이 없습니다.') != -1)
     {
         console.log(diner[2]);
         //리턴
         return;
     }else{
-        //함수화 시키기 
-        // let m = stuDiner.indexOf('아침');
+        let m = diner.indexOf('아침');
         let l = diner.indexOf('점심');
         let d = diner.indexOf('저녁');
         let s = diner.indexOf('분식');
-        for(let i = l; i<d; i++){
-            // console.log(stuDiner[i])
-            stuDinerL = stuDinerL.concat(stuDiner[i]+'\n');
+
+        if(m != -1){
+            for(let i = m; i<l; i++){
+                // console.log(stuDiner[i])
+                stuDinerM = stuDinerM.concat(diner[i]+'\n');
+            }
         }
-        for(let j = d; j<s;j++){
-            stuDinerD = stuDinerD.concat(stuDiner[j]+'\n');
+        if(l != -1){
+            for(let i = l; i<d; i++){
+                // console.log(stuDiner[i])
+                stuDinerL = stuDinerL.concat(diner[i]+'\n');
+            }
         }
-        for(let k = s; k<stuDiner.length;k++){
-            stuDinerS = stuDinerS.concat(stuDiner[k]+'\n');
+        if(d != -1){
+            if(s != -1){
+                for(let j = d; j<s;j++){
+                    stuDinerD = stuDinerD.concat(diner[j]+'\n');
+                }                
+            }
+            else{
+                for(let j = d; j<diner.length;j++){
+                    stuDinerD = stuDinerD.concat(diner[j]+'\n');
+                }
+            }
+
         }
-        // console.log(stuDinerL);
-        // console.log('\n');
-        // console.log(stuDinerD);
-        // console.log('\n');
-        // console.log(stuDinerS);
-        diner = [stuDinerL, stuDinerD, stuDinerS];
+        if(s != -1){
+            for(let k = s; k<diner.length;k++){
+                stuDinerS = stuDinerS.concat(diner[k]+'\n');
+            }
+        }
+        console.log(stuDinerL);
+        console.log('\n');
+        console.log(stuDinerD);
+        console.log('\n');
+        console.log(stuDinerS);
+        diner = "";
+        diner = [stuDinerM, stuDinerL, stuDinerD, stuDinerS];
+        // stuDinerM ="", stuDinerL="",stuDinerD="",stuDinerS="";
+        // console.log(diner);
         return diner;
     }
 
@@ -84,59 +104,29 @@ crawler(url).then(function(response){
 
         
         if(idx == 0){
-            stuDiner = strArray;
-            findDiner(stuDiner);
-            //식단찾는것 자체를 함수화 시키기 
-            /*
-            if(strArray.indexOf('등록된 식단이 없습니다.') != -1)
-            {
-                console.log(strArray[2]);
-            }else{
-                //함수화 시키기 
-                // let m = stuDiner.indexOf('아침');
-                let l = stuDiner.indexOf('점심');
-                let d = stuDiner.indexOf('저녁');
-                let s = stuDiner.indexOf('분식');
-                for(let i = l; i<d; i++){
-                    // console.log(stuDiner[i])
-                    stuDinerL = stuDinerL.concat(stuDiner[i]+'\n');
-                }
-                for(let j = d; j<s;j++){
-                    stuDinerD = stuDinerD.concat(stuDiner[j]+'\n');
-                }
-                for(let k = s; k<stuDiner.length;k++){
-                    stuDinerS = stuDinerS.concat(stuDiner[k]+'\n');
-                }
-                console.log(stuDinerL);
-                console.log('\n');
-                console.log(stuDinerD);
-                console.log('\n');
-                console.log(stuDinerS);
+            unionDiner = strArray;
+            unionDiner = findDiner(unionDiner);  
+            console.log(unionDiner);
 
-            }
-            */
         }else if(idx == 1){
             domDiner = strArray;
-            if(strArray.indexOf('등록된 식단이 없습니다.') != -1)
-            {
-                console.log(strArray[2]);
-            }
+            domDiner = findDiner(domDiner);
+            console.log(domDiner);
+
         }else if(idx == 2){
             officeDiner1 = strArray;
-            if(strArray.indexOf('등록된 식단이 없습니다.') != -1)
-            {
-                console.log(strArray[2]);
-            }
+            officeDiner1 = findDiner(officeDiner1);
+            console.log(officeDiner1);
+
         }else if(idx == 3){
             officeDiner2 = strArray;
-            if(strArray.indexOf('등록된 식단이 없습니다.') != -1)
-            {
-                console.log(strArray[2]);
-            }
+            officeDiner2 = findDiner(officeDiner2);
+            console.log(officeDiner2);
+
         }  
     })
-    fs.appendFile('test.txt',strArray,()=>{
-        console.log("save");
-    })
+    // fs.appendFile('test.txt',strArray,()=>{
+    //     console.log("save");
+    // })
     
 });
