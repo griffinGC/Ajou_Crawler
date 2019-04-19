803d6273f25db59fa55c6b30eef67f0bbda091fa
griffinGC <griffinDouble@gmail.com>
2019-04-20 01:17:05 +0900

    add comment

--- using_crawler_request.js
+++ using_crawler_request.js
@@ -2,21 +2,18 @@ const crawler = require('crawler-request');
 var cheerio = require('cheerio');
 //cheerio를 이용해서 크롤링 불가.. => crawler-request사용 
 
-const fs = require('fs');
 var url = "http://www.ajou.ac.kr/main/life/food.jsp";
 
-//enter를 없애기 위해서 삽입
-const enterReg = /(\r\n|\n|\r)/gm;
+//학생식당
 let unionDiner;
-
+//기숙사식당
 let domDiner;
-
+//교직원식당1
 let officeDiner1;
-
+//교직원식당2
 let officeDiner2;
 
 var strArray;
-const dinerList = [unionDiner, domDiner, officeDiner1, officeDiner2];
 
 //식단 찾는것 자체를 함수화
 function findDiner(diner){
@@ -27,7 +24,6 @@ function findDiner(diner){
 
     if(diner.indexOf('등록된 식단이 없습니다.') != -1)
     {
-        // console.log(diner[2]);
         //리턴
         return;
     }else{
@@ -35,7 +31,6 @@ function findDiner(diner){
         let l = diner.indexOf('점심');
         let d = diner.indexOf('저녁');
         let s = diner.indexOf('분식');
-
         //학식
         if((m== -1)&& (l!=-1) && (d == -1))
         {
@@ -51,7 +46,6 @@ function findDiner(diner){
             diner = [stuDinerL,stuDinerS];
             return diner;
         }
-
         //기식
         if((m!= -1) && (l!= -1) && (d != -1))
         {
@@ -89,14 +83,9 @@ function findDiner(diner){
     }
 
 }
-
+//크롤링시작
 crawler(url).then(function(response){
-    // console.log(response.html);
-
-    //보완필요
     var $ = cheerio.load(response.html);
-
-
     //tri_list02 이용해서 가져오면 식단 없을경우 크롤링 불가. 
     //여기는 앞뒤가 생략가능함
     $('table.ajou_table').each(function(idx){ 
@@ -111,30 +100,24 @@ crawler(url).then(function(response){
         //값이 존재하는것 들만 필터링 함
         strArray = strArray.filter(n=>n);
         // console.log(strArray);
-
-        
         if(idx == 0){
             unionDiner = strArray;
             // console.log(strArray);
             unionDiner = findDiner(unionDiner);  
             // console.log(unionDiner);
-
         }else if(idx == 1){
             domDiner = strArray;
             // console.log(strArray);
             domDiner = findDiner(domDiner);
             // console.log(domDiner);
-
         }else if(idx == 2){
             officeDiner1 = strArray;
             officeDiner1 = findDiner(officeDiner1);
             // console.log(officeDiner1);
-
         }else if(idx == 3){
             officeDiner2 = strArray;
             officeDiner2 = findDiner(officeDiner2);
             // console.log(officeDiner2);
-
         }  
     })
     console.log(unionDiner);
